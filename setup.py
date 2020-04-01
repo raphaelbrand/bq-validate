87d7527590ba268844553a03a86dbc5812ea88d1
Raphael Brand <raphaelbrand@gmail.com>
2020-04-01 10:45:08 +0200

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bq-validate',
+    version='0.1dev',
+    packages=['bqvalidate',],
+    license='The MIT License',
+    long_description=open('README.txt').read(),
+)
